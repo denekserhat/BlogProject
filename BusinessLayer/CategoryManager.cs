8ca4f8d797c6b3c8498502d17e80147e90b6f8a6
Serhat.Denek <denekserhat@gmail.com>
2019-02-21 16:52:21 +0300

    deneme123

--- BusinessLayer/CategoryManager.cs
+++ BusinessLayer/CategoryManager.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using DataAccessLayer;
+using Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BusinessLayer
 {
     public class CategoryManager
     {
+        IRepository<Category> categoryRepository;
+        public CategoryManager(IRepository<Category> _categoryRepository)
+        {
+            categoryRepository = _categoryRepository;
+        }
+
+        public async Task<List<Category>> GetCategories()
+        {
+            var returnValues = await categoryRepository.GetListAsync();
+            return returnValues;
+        }
+
+        public async Task<Category> InsertCat()
+        {
+            var returnValues = await categoryRepository.Insert();
+                return returnValues;
+        }
+
     }
 }
